141c7fee6d161a5868496d6fc0b6dd7c8be1f1ba
jaffertm <you@example.com>
2022-08-17 12:46:33 -0500

    final

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-datos = {}
+datos = {"codigo": ["001", "002", "003", "004", "005"],
+         "nombre": ["Juan", "Jose", "Yesica", "Carlos", "Daniela"],
+         "curso": ["Ingles", "Diseño web", "Matematicas", "Desarrollo personal", "Soporte de TI"]}
 
-nota1 = int(input("Ingrese la primera nota del curso: "))
-nota2 = int(input("Ingrese la segunda nota del curso: "))
-nota3 = int(input("Ingrese la tercera nota del curso: "))
-nota4 = int(input("Ingrese la cuarta nota del curso: "))
-total = nota1 + nota2 + nota3 + nota4
-promedio = total / 4
-print("El promedio es: ", promedio)
-tupla = [str(nota1), " ", str(nota2), " ", str(nota3), " ", str(nota4)]
-cadena = "".join(tupla)
-codigo = input("Ingrese el codigo del alumno: ")
-nombre = input("Ingrese el nombre del alumno: ")
-curso = input("Ingrese el nombre del curso: ")
-
-f = open("demofile.txt", "w")
-f.write("\n" + cadena)
-f.close()
+listadatos = []
+promedio = 0
+resp = "s"
+while resp == "s":
+    codigoIn = input("Ingrese el codigo del alumno: ",)
+    nombre = input("Ingrese el nombre del alumno: ")
+    curso = input("Ingrese el nombre del curso: ")
+    nota1 = int(input("Ingrese la primera nota del curso: "))
+    nota2 = int(input("Ingrese la segunda nota del curso: "))
+    nota3 = int(input("Ingrese la tercera nota del curso: "))
+    nota4 = int(input("Ingrese la cuarta nota del curso: "))
+    x = 0
+    for i in datos["codigo"]:
+        if i == codigoIn:
+            codigoTemp = i
+            nombreTemp = datos["nombre"][x]
+            cursoTemp = datos["curso"][x]
+            registro = codigoTemp, nombreTemp, cursoTemp
+            listadatos.append(registro)
+            total = nota1 + nota2 + nota3 + nota4
+            promedio = total / 4
+            print("El promedio es: ", promedio)
+            cadena = ["Codigo:" + str(codigoTemp) + " | " + "Nombre :" + str(nombreTemp) + " | " + "Curso :" + cursoTemp + " | " + "Promedio: " + str(promedio) + " | " +"Nota 1: " + str(nota1) + " | " + "Nota 2: " + str(nota2) + " | " + "Nota 3: " + str(nota3) + " | " + "Nota 4: " + str(nota4)]
+            f = open("demofile.txt", "a")
+            cadena = "".join(cadena)
+            f.write("\n" + cadena)
+            f.close()
+        x += 1
+    resp = input("¿Desea seguir ingresando datos? : s/n ")
 f = open("demofile.txt")
 print(f.read())
 f.close()
